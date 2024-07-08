a3d8bac01f9b498bd72015e9f0a0d5578076156d
TheBrodsky <stephensbrod@gmail.com>
2024-07-07 17:30:36 -0700

    I need to stop moving files and folders around

--- scenes/UI/Camera.gd
+++ scenes/UI/Camera.gd
@@ -9,7 +9,4 @@ const RETICLE_POSITION_INFLUENCE: float = 1 - PLAYER_POSITION_INFLUENCE
 
 
 func _process(delta: float) -> void:
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-		pass
-	else:
-		position = (Player.position*PLAYER_POSITION_INFLUENCE) + (Reticle.position*RETICLE_POSITION_INFLUENCE) # centered between player and reticle
+	position = (Player.position*PLAYER_POSITION_INFLUENCE) + (Reticle.position*RETICLE_POSITION_INFLUENCE) # centered between player and reticle
