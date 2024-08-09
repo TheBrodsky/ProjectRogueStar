e4e3a01032c76cea1fb262451f42cf4b09e0c246
Scott Mesdjian <scottmesdjian10@gmail.com>
2024-08-09 14:58:53 -0700

    modified SnakeAt period duration to align with Pendulum duration

--- scenes/BehaviorComponents/Targets and Followers/NewFollowers/SnakeAt/snake_at.gd
+++ scenes/BehaviorComponents/Targets and Followers/NewFollowers/SnakeAt/snake_at.gd
@@ -2,7 +2,7 @@ extends Follower
 class_name SnakeAt
 
 @export_group(Globals.MODIFIABLE_CATEGORY)
-@export var period_duration: float = 1 ## how long a complete cycle takes 
+@export var period_duration: float = 2 ## how long a complete cycle takes 
 @export var magnitude: float = 50 ## how many pixels away from the center the object goes
 
 @export_group(Globals.INSPECTOR_CATEGORY)
